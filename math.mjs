e43c4c647032760fb6dfbebc7a8f8c54a4fcf10a
Hnin-Lae <67131913+Hnin-Lae@users.noreply.github.com>
2021-12-23 15:50:49 +0900

    Add files via upload

---
+++ math.mjs
@@ -0,0 +1,18 @@
+// const PI = 3.14
+
+// export default function add(a, b) {
+//     return a + b
+// }
+// 
+const PI = 3.14
+
+function add(a, b) {
+    return a + b
+}
+
+function div(a, b) {
+    if(b === 0) return "Cannot divided by zero"
+    else return a / b
+}
+
+export { PI, add, div}
